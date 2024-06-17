3d22961ffdb5be30f9c66f5700d95c55b283ecc7
Robert <robertmarzpal@gmail.com>
2024-06-17 13:09:34 -0400

    Segunda Parte

---
+++ mi_paquete/funciones_matematicas.py
@@ -0,0 +1,25 @@
+
+frase = 'Prueba de paquete!'
+
+
+def calcular_factorial(numero):
+    factorial = 1
+    for n in range(1, (numero+1)):
+        factorial *= n
+    return factorial
+
+
+def sumar(a, b):
+    return a+b
+
+
+def restar(a, b):
+    return a-b
+
+
+def multiplicar(a, b):
+    return a*b
+
+
+def dividir(a, b):
+    return a/b
